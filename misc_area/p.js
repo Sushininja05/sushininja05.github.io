2837a6aeb65576e0bf255f5c78cdc047eba72ba9
Durdge-o <nin10dough05@gmail.com>
2026-08-28 03:31:52 -0700

    large art page addition

--- misc_area/p.js
+++ misc_area/p.js
@@ -2,6 +2,7 @@
         var confirmPassword = "the_great_vault";
         var confirmPassword2 = "infernal";
         var confirmPassword3 = "bman_ark";
+        var confirmPassword4 = "uncouth";
 		
         var password = document.getElementById("pswd").value;
         if (password == confirmPassword) {
@@ -12,5 +13,8 @@
         }
         if (password == confirmPassword3) {
              window.location="bman archive.html";
+        }
+        if (password == confirmPassword4) {
+             window.location="../art_area/art_alt.html";
         }
     }
